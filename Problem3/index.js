3585be9b5d23b45143edf9e27502eebb4c90696e
Markfady <markfady9@gmail.com>
2023-05-24 22:15:10 +0300

    sum all data except the highest,lowest values

--- Problem3/index.js
+++ Problem3/index.js
@@ -1,10 +1,15 @@
 function exclude_lowest_highest(arr){
     if(arr==null) return 0;
-    let lowestValue=Math.min(...arr);
+    //solution 1
+   /*  let lowestValue=Math.min(...arr);
     let highestValue=Math.max(...arr);
     let newArray=arr.filter((x)=>x!==lowestValue && x!==highestValue).reduce((acc,current)=>acc+current,0);
-    console.log(newArray)
+    console.log(newArray) */
+
+//solution2  In this solution will only remove the repeated number from lowest or highest 1 time only not both like solution1 
+return arr.sort((a,b)=>a-b).slice(1,-1).reduce((acc,current)=>acc+current,0);
+
 }
 
-exclude_lowest_highest([100,200,400,300])   
+console.log(exclude_lowest_highest([1,1,2,3]))   
 //Will remove the highest value 'not index' (400) and lowest value(100) and sum the other 
