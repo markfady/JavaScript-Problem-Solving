abd4abb68deecf4e3316e2f07eb286ac59d5ebab
Markfady <markfady9@gmail.com>
2023-05-24 21:12:53 +0300

    summ all data except the highest,lowest values

---
+++ Problem3/index.js
@@ -0,0 +1,9 @@
+function exclude_lowest_highest(arr){
+    if(arr==null) return 0;
+    let lowestValue=Math.min(...arr);
+    let highestValue=Math.max(...arr);
+    let newArray=arr.filter((x)=>x!==lowestValue && x!==highestValue).reduce((acc,current)=>acc+current,0);
+    console.log(newArray)
+}
+
+exclude_lowest_highest([100,200,400,300])   //Will remove the highest value 'not index' (400) and lowest value(100) and sum the other 
