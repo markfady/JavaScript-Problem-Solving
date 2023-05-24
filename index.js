ab01851cd985f7b4bab986b6f39d1eb854b46596
Markfady <markfady9@gmail.com>
2023-05-24 18:56:46 +0300

    sum of positive values

---
+++ index.js
@@ -0,0 +1,12 @@
+function sum_of_positives(arr){
+    let defaultvalue=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>0){
+            consoloe.log(defaultvalue+=arr[i])
+        }
+        else{
+            return console.log(defaultvalue)
+        }
+    }
+}
+sum_of_positives([1,-1,2]);
