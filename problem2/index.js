e1c4b73dd095c0d99cdf149c25d13ef855ce7a2d
Markfady <markfady9@gmail.com>
2023-05-24 19:15:27 +0300

    sum of positive values

--- problem2/index.js
+++ problem2/index.js
@@ -1,12 +1,14 @@
 function sum_of_positives(arr){
-    let defaultvalue=0;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>0){
-            consoloe.log(defaultvalue+=arr[i])
-        }
-        else{
-            return console.log(defaultvalue)
-        }
-    }
-}
-sum_of_positives([1,-1,2]);
+   //Solution 1
+                            /*  let defaultvalue=0;
+                                for(let i=0;i<arr.length;i++){
+                                    if(arr[i]>0){
+                                    defaultvalue+=arr[i]
+                                    }
+                                }
+                                return defaultvalue;
+                            */
+//Solution 2 using .filter which takes our array as input and return new array using our condition + reduce where acc=current item ,current=next item,0=start from 1st index
+return arr.filter((x)=>x>0).reduce((acc,current)=>acc+current,0)
+                            }
+console.log(sum_of_positives([1,-3,2]))
