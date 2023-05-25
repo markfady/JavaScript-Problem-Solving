a18d4c33cd2dd6ae5d3b14280a05e52177a1a8cd
Markfady <markfady9@gmail.com>
2023-05-26 02:06:01 +0300

    Counting sheep

---
+++ Problem6/index.js
@@ -0,0 +1,13 @@
+function presentsheep(arr){
+
+    let i=0;
+    arr.map((x)=>{
+    if (x=="true"){     
+        i+=1;
+    }
+});
+console.log(i)
+}
+presentsheep(["true","true","true","false","true","false","true",
+            "true","true","true","false","true","false","true",
+            "true","true","true","false","true","false","true"])
