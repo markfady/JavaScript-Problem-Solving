15db085240bc2b6117637ff11c7b5cb3af1b088f
Markfady <markfady9@gmail.com>
2023-05-26 01:17:39 +0300

    reverse entered  numbers in array

---
+++ Problem5/index.js
@@ -0,0 +1,13 @@
+function reversenumber(numbers){
+    /*solution 1
+let values=numbers.toString().split('') //convert number to string so we can split them into new array
+let array=values.reverse()  //after split we can reverse them
+return   array.map((x) => {       //loop on each item 
+return   parseInt(x);      //to convert them back into numbers
+})
+*/
+
+//solution 2 
+return numbers.toString().split('').map((x)=>Number(x)).reverse();
+}
+console.log(reversenumber(54321))
