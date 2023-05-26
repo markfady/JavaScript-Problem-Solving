dc2904717fbe6730f6659c33a4423a687d9657b5
Markfady <markfady9@gmail.com>
2023-05-26 18:33:22 +0300

    Count +Ve & Sum -ve

--- 11-Problem11/index.js
+++ 11-Problem11/index.js
@@ -1,4 +1,16 @@
-function Countpositive_sumNegative(){
+function Countpositive_sumNegative(arr){
+    let sum=0;
+    let positiveNumbers=[]
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]<0){
+        sum+=arr[i]
+    }
+    else{
+        positiveNumbers.push(arr[i]);
 
-    
+    }
+        
+    }
+    return [sum , positiveNumbers.length]
 }
+console.log  (Countpositive_sumNegative([5,-2,-5,-9,8,9,-1]))
