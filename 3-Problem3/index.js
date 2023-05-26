0ef1b3d3afb08780e31ee8829b16951798a5e7fc
Markfady <markfady9@gmail.com>
2023-05-26 17:49:25 +0300

    Sum All  Without Highest & Lowest

--- 3-Problem3/index.js
+++ 3-Problem3/index.js
@@ -8,8 +8,6 @@ function exclude_lowest_highest(arr){
 
 //solution2  In this solution will only remove the repeated number from lowest or highest 1 time only not both like solution1 
 return arr.sort((a,b)=>a-b).slice(1,-1).reduce((acc,current)=>acc+current,0);
-
 }
-
 console.log(exclude_lowest_highest([1,1,2,3]))   
 //Will remove the highest value 'not index' (400) and lowest value(100) and sum the other 
