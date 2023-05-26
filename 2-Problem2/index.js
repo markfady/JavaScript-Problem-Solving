8d694c2ba657ea8b011ebae34591f445383bd5a2
Markfady <markfady9@gmail.com>
2023-05-26 17:48:21 +0300

    Sum of Positives

--- 2-Problem2/index.js
+++ 2-Problem2/index.js
@@ -10,5 +10,5 @@ function sum_of_positives(arr){
                             */
 //Solution 2 using .filter which takes our array as input and return new array using our condition + reduce where acc=current item ,current=next item,0=start from 1st index
 return arr.filter((x)=>x>0).reduce((acc,current)=>acc+current,0)
-                            }
+                        }
 console.log(sum_of_positives([1,-3,2]))
