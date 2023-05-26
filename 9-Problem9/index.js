3fabddc33660780f8d4801f3820f17a5a29366d3
Markfady <markfady9@gmail.com>
2023-05-26 17:56:05 +0300

    Jenny&Jhonny

--- 9-Problem9/index.js
+++ 9-Problem9/index.js
@@ -1,5 +1,5 @@
 function Jenny_Jhonny(name){
-    return name!=="Johnny" ? "Hello"+name+"!" : "Hello,my love"; 
+     return name!=="Johnny" ? "Hello"+name+"!" : "Hello,my love"; 
 }
 console.log(Jenny_Jhonny("John"))
 console.log(Jenny_Jhonny("Johnny"))
