1ee9aad3eb72a09e8a87478c986ad24b8806ca2a
Markfady <markfady9@gmail.com>
2023-05-28 18:59:19 +0300

    Remove last Exclamation Mark

--- 29-Problem29/index.js
+++ 29-Problem29/index.js
@@ -1,4 +1,17 @@
 function removeExclamation(str){  
+        /* solution  1
+    if(str.endsWith("!")){
+        return str.slice(0,str.length-1)
+    }  else{
+        return str;
+    }   
+    */
+    
+    //solution 2 
     return str.replace(/!$/,"")
+
+
+
+
 }
 console.log(removeExclamation("!HI!!"))
