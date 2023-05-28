8755d7d2b4c3d0209426120a1991aedbc20577e1
Markfady <markfady9@gmail.com>
2023-05-28 20:05:51 +0300

    Solve timmy forloop bug

---
+++ 32-Problem32/index.js
@@ -0,0 +1,8 @@
+function unFinishedLoopFix(number){
+        var array=[]
+        for(let i=0;i<=number;i++){
+            array.push(i)
+        }
+        return array
+}
+console.log(unFinishedLoopFix([8]))
