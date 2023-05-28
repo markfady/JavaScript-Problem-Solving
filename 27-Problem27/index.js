84a20a8e477150896f62fca879df9748f78ed834
Markfady <markfady9@gmail.com>
2023-05-28 15:17:31 +0300

    Number to string Switch

---
+++ 27-Problem27/index.js
@@ -0,0 +1,38 @@
+function numberName(str){
+
+    switch(str){
+        case  0 :
+        return "zero";
+        break;
+        case  1 :
+        return "one";
+        break;
+        case  2 :
+        return "two";
+        break;
+        case  3 :
+        return "three";
+        break;
+        case  4 :
+        return "four";
+        break;
+        case  5 :
+        return "five";
+        break;
+        case  6 :
+        return "six";
+        break;
+        case  7 :
+        return "seven";
+        break;
+        case  8 :
+        return "eight";
+        break;
+        case  9 :
+        return "nine";
+        break;
+        default: return "Enter numbers from 0 to 9"
+
+    }
+}
+console.log(numberName(10));
