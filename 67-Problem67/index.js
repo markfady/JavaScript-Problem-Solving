2d6b24e7e9202cbf09a0ac597e1addd7be303779
Markfady <markfady9@gmail.com>
2023-06-03 20:23:58 +0300

    Owns or Not

---
+++ 67-Problem67/index.js
@@ -0,0 +1,4 @@
+function ownOrnot(name="Mark"){
+    return name=="zach" ? `${name} owns 18` : `${name} owns zero`}
+    console.log(ownOrnot("zach"))
+    console.log(ownOrnot()) //default value will be passed
