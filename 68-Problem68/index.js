30cfed2a2de9e68abffa8bd5a0fd13e65eef495a
Markfady <markfady9@gmail.com>
2023-06-03 20:28:50 +0300

    How many Father's age twice his son

---
+++ 68-Problem68/index.js
@@ -0,0 +1,6 @@
+function twiceSon(Father,son){
+    return Math.abs( Father-(son*2)) //How many years ago father was twice his son age
+    
+}
+console.log(twiceSon(37,7))
+console.log(twiceSon(50,30))
